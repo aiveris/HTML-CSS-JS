c0a9a28f824f1404f7a529b9c4afa96960982583
aiveris <staind0323>
2019-09-14 15:50:50 +0300

    2019-09-14

--- src/main/java/cf/mindaugas/model/Driver.java
+++ src/main/java/cf/mindaugas/model/Driver.java
@@ -22,13 +22,49 @@ public class Driver {
         // simpleEntityExample();
 
         // 2 ex: Basic Hibernate types:
-        // basicTypesExample();
+        basicTypesExample();
 
         // 3 ex: persisting (saving) Java collections
-        persistingCollections();
+        // persistingCollections();
 
+        // 4. ex: read and update entity
+        // simpleEntityReadAndUpdate();
+        // 5. ex: delete an entity
+        // System.out.println("Entity should be in the database, please check ... I will sleep for 10s");
+        // Thread.sleep(10000);
+        // simpleEntityDelete();
+        // System.out.println("Entity should be deleted!");
     }
 
+    public static void simpleEntityExample(){
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build(); // Create registry
+        MetadataSources sources = new MetadataSources(registry); // Create MetadataSources
+        Metadata metadata = sources.getMetadataBuilder().build(); // Create Metadata
+
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build(); // Create SessionFactory
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        System.out.println("----- Transaction has begun ----------");
+
+        // CREATE
+        User user = new User("Daniel", "Joseph");
+        session.persist(user);
+
+        // How can we check whether the entity is persisted
+        // during the persist() call or during the commit()?
+
+        // System.out.println("----- Check now 1 -----");
+        // Thread.sleep(10000);
+
+        transaction.commit();
+
+        // System.out.println("----- Check now 2 -----");
+        // Thread.sleep(10000);
+
+        session.close();
+        sessionFactory.close();
+    }
     public static void basicTypesExample() throws MalformedURLException {
 
         StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build(); // Create registry
@@ -50,36 +86,6 @@ public class Driver {
         transaction.commit();
         session.close();
     }
-
-    public static void simpleEntityExample(){
-        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build(); // Create registry
-        MetadataSources sources = new MetadataSources(registry); // Create MetadataSources
-        Metadata metadata = sources.getMetadataBuilder().build(); // Create Metadata
-
-        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build(); // Create SessionFactory
-        Session session = sessionFactory.openSession();
-        Transaction transaction = session.beginTransaction();
-
-        System.out.println("----- Transaction has begun ----------");
-
-        // CREATE
-        User user = new User("Robert", "Martin");
-        session.persist(user);
-
-        // How can we check whether the entity is persisted
-        // during the persist() call or during the commit()?
-
-        // System.out.println("----- Check now 1 -----");
-        // Thread.sleep(10000);
-
-        transaction.commit();
-
-        // System.out.println("----- Check now 2 -----");
-        // Thread.sleep(10000);
-
-        session.close();
-        sessionFactory.close();
-    }
     public static void persistingCollections(){
         StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build(); // Create registry
         MetadataSources sources = new MetadataSources(registry); // Create MetadataSources
@@ -90,15 +96,55 @@ public class Driver {
         Transaction transaction = session.beginTransaction();
 
         ArrayList<String> phones = new ArrayList<>();
-        phones.add("+77 889 008033");
-        phones.add("+370 84 08977777");
+        phones.add("+370 889 008033");
+        phones.add("+370 456 977777");
 
         // CREATE
-        UserWPhone userWPhone = new UserWPhone("Alexardr", "Bell");
+        UserWPhone userWPhone = new UserWPhone("Alexander", "Bell");
         userWPhone.setPhones(phones);
         session.persist(userWPhone);
 
         transaction.commit();
         session.close();
+    }
+    public static void simpleEntityReadAndUpdate(){
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build(); // Create registry
+        MetadataSources sources = new MetadataSources(registry); // Create MetadataSources
+        Metadata metadata = sources.getMetadataBuilder().build(); // Create Metadata
+
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build(); // Create SessionFactory
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        // UPDATE
+        User user = session.find(User.class, 1L); // NullPointerException if object is not found
+        user.setName("Robert H.");
+        transaction.commit();
+        session.close();
+    }
+    public static void simpleEntityDelete() throws InterruptedException {
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build(); // Create registry
+        MetadataSources sources = new MetadataSources(registry); // Create MetadataSources
+        Metadata metadata = sources.getMetadataBuilder().build(); // Create Metadata
+
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build(); // Create SessionFactory
+        Session session = sessionFactory.openSession();
+        Transaction transaction = session.beginTransaction();
+
+        // CREATE
+        User user = new User("Robert", "Martin");
+        session.persist(user);
+        transaction.commit();
+
+        System.out.println("Entity should be in the database, please check ... I will sleep for 10s");
+        Thread.sleep(10000);
+
+        // DELETE
+        Transaction transaction2 = session.beginTransaction();
+        session.delete(session.find(User.class, 1L));
+        transaction2.commit();
+
+        session.close();
+        sessionFactory.close();
     }
 }
